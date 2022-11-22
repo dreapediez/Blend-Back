3aed888b89feda3eaab179e50dd95bd21307e0f2
Andrea Pedreño <apedrenodiez@gmail.com>
2022-11-22 14:35:02 +0100

    Add user register controller test and their correspondig mock file

---
+++ src/mocks/userMocks.ts
@@ -0,0 +1,19 @@
+import type { UserCredentials } from "../server/types/userTypes";
+
+export const userMock: UserCredentials = {
+  username: "leo",
+  password: "leo123",
+};
+
+export const userMockWithId = {
+  username: "leo",
+  password: "leo123",
+  _id: "155654rrd",
+};
+
+export const userMockCredentials = {
+  username: "leo",
+  password: "leo123",
+  email: "leo@gmail.com",
+  id: "ñasokdfjdsñlk",
+};
---
+++ src/server/controllers/usersControllers.test.ts
@@ -0,0 +1,55 @@
+import type { NextFunction, Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import CustomError from "../../CustomError/CustomError.js";
+import { userMockCredentials } from "../../mocks/userMocks.js";
+import registerUser from "./usersControllers.js";
+import User from "../../mongodb/models/User.js";
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+const res: Partial<Response> = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+};
+
+const next = jest.fn().mockReturnThis();
+
+describe("Given a registerUser Controller", () => {
+  const req: Partial<Request> = {
+    body: userMockCredentials,
+  };
+
+  describe("When it receives a request with username 'leo', password: 'leo123' and a response", () => {
+    test("Then it should return the response method status 201", async () => {
+      const expectedStatus = 201;
+
+      const hashedPassword = await bcrypt.hash(
+        userMockCredentials.password,
+        10
+      );
+
+      bcrypt.hash = jest.fn().mockResolvedValue(hashedPassword);
+
+      User.create = jest
+        .fn()
+        .mockReturnValue({ ...userMockCredentials, password: hashedPassword });
+
+      await registerUser(req as Request, res as Response, null);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
+      expect(res.json).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it receives a request and there is an error", () => {
+    test("Then it should call next with a custom Error", async () => {
+      User.create = jest.fn().mockRejectedValue(new CustomError("", 500, ""));
+
+      await registerUser(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+});
