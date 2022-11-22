21052f2b143ea727da15796988ab4f000b6d14e4
Andrea Pedreño <apedrenodiez@gmail.com>
2022-11-22 12:11:56 +0100

    Add cors configuration

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,8 +1,19 @@
 import "../loadEnvironments.js";
+import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "https://202209-final-project-andrea-pedreno.netlify.app",
+      "http://localhost:3000",
+      "http://localhost:4000",
+    ],
+  })
+);
 
 app.disable("x-powered-by");
 
